9d5dbaeea39db3116d774f2abee66d172363fce1
Mike Keller <kontakt@mike-keller.de>
2022-11-26 11:01:34 +0100

    Initial Commit

---
+++ Expense Tracker/Expense_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Expense_TrackerApp.swift
+//  Expense Tracker
+//
+//  Created by Mike Keller on 26.11.22.
+//
+
+import SwiftUI
+
+@main
+struct Expense_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
